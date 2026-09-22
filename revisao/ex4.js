a0e0c4e80c0d9fc7e967b025c629608ea52bd843
Nicolas-senai-2026 <dichava190@gmail.com>
2026-09-22 16:59:44 -0300

    revisao 22/09

---
+++ revisao/ex4.js
@@ -0,0 +1,27 @@
+const entrada = require('readline-sync');
+
+const materiais = [];
+
+for (let i = 0; i < 4; i++) {
+    const material = {
+        nome: entrada.question(`Digite o nome do ${i + 1}o material: `), quantidade: entrada.questionInt(`Digite a quantidade do ${i + 1}o material: `), estoqueMinimo: entrada.questionInt(`Digite o estoque minimo: `)
+    };
+    materiais.push(material);
+}
+console.log("\n=== RELATORIO DE ESTOQUE ===");
+
+for (let i = 0; i < materiais.length; i++) {
+    const material = materiais[i];
+
+    let situacao;
+    if (material.quantidade < material.estoqueMinimo) {
+        situacao = "ESTOQUE BAIXO";
+    } else {
+        situacao = "ESTOQUE OK";
+    }
+    console.log(`Material: ${material.nome}`);
+    console.log(`Quantidade: ${material.quantidade}`);
+    console.log(`Estoque Minimo: ${material.estoqueMinimo}`);
+    console.log(`Situacao: ${situacao}`);
+    console.log("-".repeat(20));
+}
